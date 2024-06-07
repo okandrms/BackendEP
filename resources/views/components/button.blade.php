d3012a5797d1167aa0a287007a3b2dbb18df4243
okandrms <okanndrms@gmail.com>
2024-06-07 21:41:24 +0200

    styled buttons and added Alpinejs

---
+++ resources/views/components/button.blade.php
@@ -0,0 +1,5 @@
+
+<button
+  {{ $attributes->class(['rounded-md border-2 border-grey bg-white px-2.5 py-1.5 text-center text-sm font-semibold text-black shadow-sm hover:bg-slate-100 overflow-hidden']) }}>
+  {{ $slot }}
+</button>
