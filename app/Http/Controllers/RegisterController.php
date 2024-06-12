471f364a9a1244e4d195063d8b03151ad91e3980
okandrms <okanndrms@gmail.com>
2024-06-12 22:59:34 +0200

    Registration fixed

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -4,18 +4,20 @@ namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
 use App\Models\User;
-use Illuminate\Foundation\Auth\RegistersUsers;
+use Illuminate\Foundation\RegistersUsers;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Support\Facades\Auth;
+//RouteServicePRovider
+use App\Providers\RouteServiceProvider;
 // request
 use Illuminate\Http\Request;
 
 class RegisterController extends Controller
 {
-    use RegistersUsers;
+    // use RegistersUsers;
 
-    protected $redirectTo = '/home';
+    // protected $redirectTo = 'jobs.index';
 
     public function __construct()
     {
@@ -32,17 +34,29 @@ class RegisterController extends Controller
         return Validator::make($data, [
             'name' => ['required', 'string', 'max:255'],
             'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
-            'password' => ['required', 'string', 'min:8', 'confirmed'],
+            'password' => ['required', 'string', 'confirmed'],
         ]);
     }
 
     protected function create(Request $request)
-    {
-       // dd($request->all());
-        return User::create([
-            'name' => $request['name'],
-            'email' => $request['email'],
-            'password' => Hash::make($request['password']),
-        ]);
+{
+    // Validate the request
+    $this->validator($request->all())->validate();
+
+    // Create the user
+    $user = User::create([
+        'name' => $request['name'],
+        'email' => $request['email'],
+        'password' => Hash::make($request['password']),
+    ]);
+
+    // Attempt to log the user in
+    if (Auth::attempt(['email' => $request['email'], 'password' => $request['password']])) {
+        // If successful, redirect to /home
+        return redirect()->intended('/');
     }
+
+    // If authentication fails, you can handle it here (optional)
+    return redirect()->route('login')->with('error', 'Registration successful, but login failed.');
+}
 }
